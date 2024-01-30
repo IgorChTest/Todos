ad6674ad0fed21152629c35212864a2a9c039d04
IgorCH QA <ic641716@gmail.com>
2024-01-30 12:30:45 +0300

    Add test with several matches

--- src/test/java/ru/netology/TodosTest.java
+++ src/test/java/ru/netology/TodosTest.java
@@ -73,4 +73,26 @@ public class TodosTest {
         Task[] actual = todos.search("ДЗ");
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldReturnAnswerWithSeveralMatches() {
+        SimpleTask simpleTask = new SimpleTask(1, "Сделать баг-репорты 3й версии приложения");
+        Epic epic = new Epic(2, new String[]{"Smoke test приложения"});
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.search("приложения");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
